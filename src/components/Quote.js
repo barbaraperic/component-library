e67e67c30d9cc88c086739cef1caa1423e573dc8
Barbara Peric <b.peric10@gmail.com>
2021-04-05 16:36:18 +0200

    WIP: quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,74 +1,72 @@
-const Quote = ({ by, source, children }) => {
-  return (
-    <figure>
-      <QuoteContent>{children}</QuoteContent>
-      <figcaption>
-        <Author>
-          <SourceLink href={source}>{by}</SourceLink>
-        </Author>
-      </figcaption>
-    </figure>
-  );
-};
+// import React from 'react';
+// import { styled } from 'styled-components';
 
-/*
-  We want this TextLink to be black
-  and underlined when it's inside
-  a Quote component.
-*/
+// const Quote = ({ by, source, children }) => {
+//   return (
+//     <figure>
+//       <QuoteContent>{children}</QuoteContent>
+//       <figcaption>
+//         <Author>
+//           <SourceLink href={source}>{by}</SourceLink>
+//         </Author>
+//       </figcaption>
+//     </figure>
+//   );
+// };
 
 
-const QuoteContent = styled.blockquote`
-  margin: 0;
-  background: hsl(0deg 0% 90%);
-  padding: 16px 20px;
-  border-radius: 8px;
-  font-style: italic;
 
-  &:before {
-    content: '“';
-  }
+// const QuoteContent = styled.blockquote`
+//   margin: 0;
+//   background: hsl(0deg 0% 90%);
+//   padding: 16px 20px;
+//   border-radius: 8px;
+//   font-style: italic;
 
-  &:after {
-    content: '”';
-  }
-`;
+//   &:before {
+//     content: '“';
+//   }
 
-const TextLink = styled.a`
-  color: blue;
-  text-decoration: none;
+//   &:after {
+//     content: '”';
+//   }
+// `;
+
+// const TextLink = styled.a`
+//   color: blue;
+//   text-decoration: none;
   
-  ${QuoteContent} & {
-     color: black;
+//   ${QuoteContent} & {
+//      color: black;
     
-    &:hover {
-      text-decoration: underline; 
-  }
-`;
+//     &:hover {
+//       text-decoration: underline; 
+//   }
+// `;
 
-/* You can safely ignore everything below this point! It doesn't need to change */
-const Author = styled.cite`
-  display: block;
-  text-align: right;
-  margin-top: 8px;
-`;
+// /* You can safely ignore everything below this point! It doesn't need to change */
+// const Author = styled.cite`
+//   display: block;
+//   text-align: right;
+//   margin-top: 8px;
+// `;
 
-const SourceLink = styled.a`
-  text-decoration: none;
-  color: hsl(0deg 0% 35%);
+// const SourceLink = styled.a`
+//   text-decoration: none;
+//   color: hsl(0deg 0% 35%);
 
-  &:before {
-    content: '—';
-  }
-`;
+//   &:before {
+//     content: '—';
+//   }
+// `;
 
-render(
-  <>
-    <Quote by="Unknown" source="/">
-      This quote <TextLink href="/">contains a link</TextLink>!
-    </Quote>
-    <p>
-      This paragraph <TextLink href="/">contains a link</TextLink>!
-    </p>
-  </>
-);
+// render(
+//   <>
+//     <Quote by="Unknown" source="/">
+//       This quote <TextLink href="/">contains a link</TextLink>!
+//     </Quote>
+//     <p>
+//       This paragraph <TextLink href="/">contains a link</TextLink>!
+//     </p>
+//   </>
+// );
