025cdb2f53c56674069c9d5fc12b464d557de98b
Barbara Peric <b.peric10@gmail.com>
2021-05-02 16:45:40 +0200

    default theme

---
+++ utils/theme.js
@@ -0,0 +1,12 @@
+import { primaryFont } from "./typography";
+import { blue, neutral } from "./colors";
+
+export const defaultTheme = {
+  primaryColor: blue[300],
+  primaryColorHover: blue[200],
+  primaryColorActive: blue[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont
+};
